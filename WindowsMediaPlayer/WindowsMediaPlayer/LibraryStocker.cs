30e814a5ed9300b936947b7fc8b01f76aaa42360
Vroenguard <corentin.bajdek@gmail.com>
2015-01-11 20:33:02 +0100

    Version pseudo finale
    Version pseudo finale

---
+++ WindowsMediaPlayer/WindowsMediaPlayer/LibraryStocker.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.IO;
+
+
+    class LibraryStocker
+    {
+        protected List<string> listMusic = new List<string>();
+        protected List<string> listPic = new List<string>();
+        protected List<string> listVideo = new List<string>();
+        protected bool         isLoad;
+
+        public enum FileType
+        {
+            Video,
+            Picture,
+            Music,
+            Other
+        }
+
+        public LibraryStocker()
+        {
+            isLoad = false;
+        }
+
+        public bool  loadFromXml()
+        {
+            if (isLoad == true)
+                return (true);
+            XmlSerializer SerializerObj = new XmlSerializer(typeof(List<string>));
+            FileStream ReadFileStream1 = new FileStream("ListMusic.txt", FileMode.Open, FileAccess.Read, FileShare.Read);
+            FileStream ReadFileStream2 = new FileStream("ListPicture.txt", FileMode.Open, FileAccess.Read, FileShare.Read);
+            FileStream ReadFileStream3 = new FileStream("ListVideo.txt", FileMode.Open, FileAccess.Read, FileShare.Read);
+
+            var tmp1 = (List<string>)SerializerObj.Deserialize(ReadFileStream1);
+            var tmp2 = (List<string>)SerializerObj.Deserialize(ReadFileStream2);
+            var tmp3 = (List<string>)SerializerObj.Deserialize(ReadFileStream3);
+
+            listMusic.AddRange(tmp1);
+            listPic.AddRange(tmp2);
+            listVideo.AddRange(tmp3);
+
+            ReadFileStream1.Close();
+            ReadFileStream2.Close();
+            ReadFileStream3.Close();
+            isLoad = true;
+            return (isLoad);
+        }
+
+        public List<string> getMusic()
+        {
+            if (isLoad == false)
+                loadFromXml();
+            return (listMusic);
+        }
+
+        public List<string> getPic()
+        {
+            if (isLoad == false)
+                loadFromXml();
+            return (listPic);
+        }
+
+        public List<string> getVideo()
+        {
+            if (isLoad == false)
+                loadFromXml();
+            return (listVideo);
+        }
+
+        public static FileType getFileType(string param)
+        {
+            string[] extVideo = { ".mp4", ".avi", ".wmv", ".mkv" };
+            string[] extPic = { ".jpg", ".bmp", ".dib", ".jpeg", ".jpe", ".gif", ".png"};
+            string[] extMusic = { ".mp3", ".wma", ".wav", ".ogg", ".m4p", ".3gp"};
+
+            for (int i = 0; i < extVideo.Length; i++)
+                if (extVideo[i] == param.Substring(param.LastIndexOf('.')))
+                    return (FileType.Video);
+            for (int i = 0; i < extPic.Length; i++)
+                if (extPic[i] == param.Substring(param.LastIndexOf('.')))
+                    return (FileType.Picture);
+            for (int i = 0; i < extMusic.Length; i++)
+                if (extMusic[i] == param.Substring(param.LastIndexOf('.')))
+                    return (FileType.Music);
+            return (FileType.Other);
+        }
+
+        protected void updateXml(FileType param)
+        {
+            XmlSerializer SerializerObj = new XmlSerializer(typeof(List<string>));
+            TextWriter WriteFileStream;
+
+            if (param == FileType.Other)
+                return ;
+            if (param == FileType.Music)
+            {
+                WriteFileStream = new StreamWriter("ListMusic.txt");
+                SerializerObj.Serialize(WriteFileStream, listMusic);
+            }
+            else if (param == FileType.Video)
+            {
+                WriteFileStream = new StreamWriter("ListVideo.txt");
+                SerializerObj.Serialize(WriteFileStream, listVideo);
+            }
+            else
+            {
+                WriteFileStream = new StreamWriter("ListPicture.txt");
+                SerializerObj.Serialize(WriteFileStream, listPic);
+            }
+            WriteFileStream.Close();
+        }
+
+        public void addMedia(string param)
+        {
+            FileType fileType = getFileType(param);
+
+            if (isLoad == false)
+                loadFromXml();
+            if (fileType == FileType.Video && listVideo.Contains(param) == false)
+                   listVideo.Add(param);
+            if (fileType == FileType.Picture && listPic.Contains(param) == false)
+                    listPic.Add(param);
+            if (fileType == FileType.Music && listMusic.Contains(param) == false)
+                    listMusic.Add(param);
+            updateXml(fileType);
+        }
+
+        public void removeMedia(string param)
+        {
+            FileType fileType = getFileType(param);
+
+            if (fileType == FileType.Other)
+                return;
+
+            if (fileType == FileType.Video)
+            { 
+                for (int i = 0; i < listVideo.Count(); i++)
+                    if (listVideo.ElementAt(i).Contains(param))
+                        listVideo.Remove(listVideo.ElementAt(i));
+            }
+            else if (fileType == FileType.Music)
+            {
+                for (int i = 0; i < listMusic.Count(); i++)
+                    if (listMusic.ElementAt(i).Contains(param))
+                        listMusic.Remove(listMusic.ElementAt(i));
+            }
+            else if (fileType == FileType.Picture)
+            {
+                for (int i = 0; i < listPic.Count(); i++)
+                    if (listPic.ElementAt(i).Contains(param))
+                        listPic.Remove(listPic.ElementAt(i));
+            }
+          updateXml(fileType);
+        }
+
+        public string   getfullpath(string param)
+        {
+            FileType fileType = getFileType(param);
+
+            if (fileType == FileType.Other)
+                return ("error");
+
+            if (fileType == FileType.Video)
+            {
+                for (int i = 0; i < listVideo.Count(); i++)
+                    if (listVideo.ElementAt(i).Contains(param))
+                        return (listVideo.ElementAt(i));
+            }
+            else if (fileType == FileType.Music)
+            {
+                for (int i = 0; i < listMusic.Count(); i++)
+                    if (listMusic.ElementAt(i).Contains(param))
+                        return (listMusic.ElementAt(i));
+            }
+            else if (fileType == FileType.Picture)
+            {
+                for (int i = 0; i < listPic.Count(); i++)
+                    if (listPic.ElementAt(i).Contains(param))
+                        return (listPic.ElementAt(i));
+            }
+            return ("error");
+        }
+    }
--- WindowsMediaPlayer/WindowsMediaPlayer/MainWindow.xaml.cs
+++ WindowsMediaPlayer/WindowsMediaPlayer/MainWindow.xaml.cs
@@ -33,6 +33,7 @@ namespace WindowsMediaPlayer
         private bool            MediaReader_Playing = false;
         private bool            userIsDraggingSlider = false;
         private List<string>    _playlist_items = new List<string>();
+        private LibraryStocker  LibStocker = new LibraryStocker();
 
         private ImageBrush brush_play;
         private ImageBrush brush_pause;
@@ -40,6 +41,8 @@ namespace WindowsMediaPlayer
         private ImageBrush brush_stop;
         private ImageBrush brush_next;
         private ImageBrush brush_prev;
+        private ImageBrush brush_delete;
+        private ImageBrush brush_send;
 
         public MainWindow()
         {
@@ -51,6 +54,8 @@ namespace WindowsMediaPlayer
             brush_stop = new ImageBrush();
             brush_next = new ImageBrush();
             brush_prev = new ImageBrush();
+            brush_delete = new ImageBrush();
+            brush_send = new ImageBrush();
 
             brush_play.ImageSource = new BitmapImage(new Uri("play_button.png", UriKind.Relative));
             brush_pause.ImageSource = new BitmapImage(new Uri("pause_button.png", UriKind.Relative));
@@ -58,12 +63,16 @@ namespace WindowsMediaPlayer
             brush_stop.ImageSource = new BitmapImage(new Uri("stop_button.png", UriKind.Relative));
             brush_next.ImageSource = new BitmapImage(new Uri("next_button.png", UriKind.Relative));
             brush_prev.ImageSource = new BitmapImage(new Uri("previous_button.png", UriKind.Relative));
+            brush_delete.ImageSource = new BitmapImage(new Uri("delete_button.png", UriKind.Relative));
+            brush_send.ImageSource = new BitmapImage(new Uri("send_button.png", UriKind.Relative));
 
             My_Button_Play.Background = brush_play;
             My_Button_Load.Background = brush_load;
             My_Button_Stop.Background = brush_stop;
             My_Button_Next.Background = brush_next;
             My_Button_Prev.Background = brush_prev;
+            My_Button_Delete.Background = brush_delete;
+            My_Button_Send.Background = brush_send;
             if (My_Playist.Items.Count == 0)
                 My_Text_No_Playlist.Content = "Your playlist" + Environment.NewLine + "   is empty.";
 
@@ -71,15 +80,24 @@ namespace WindowsMediaPlayer
             timer.Interval = TimeSpan.FromSeconds(1);
             timer.Tick += timer_Tick;
             timer.Start();
+
+            Bibli_reset();
         }
 
         private void timer_Tick(object sender, EventArgs e)
         {
+            if (_playlist_items.Count >= 1)
+                My_Text_No_Playlist.Content = "";
+
             if ((MediaReader.Source != null) && (MediaReader.NaturalDuration.HasTimeSpan) && (!userIsDraggingSlider))
             {
                 sliProgress.Minimum = 0;
                 sliProgress.Maximum = MediaReader.NaturalDuration.TimeSpan.TotalSeconds;
                 sliProgress.Value = MediaReader.Position.TotalSeconds;
+
+                //s'il n'a pas bien démaré
+                if (MediaReader.Position.TotalSeconds == 0 && MediaReader_Playing == true)
+                    MediaReader.Play();
 
                 // si la vidéo est finie
                 if (MediaReader.Position.TotalSeconds == MediaReader.NaturalDuration.TimeSpan.TotalSeconds)
@@ -114,26 +132,19 @@ namespace WindowsMediaPlayer
                 return ;
             MediaReader.Source = new Uri(ofd.FileName);
 
-            //playlist related
-            My_Playist.Items.Insert(0, ofd.FileName.Substring(ofd.FileName.LastIndexOf('\\') + 1));
-            _playlist_items.Insert(0, ofd.FileName);
-            My_Playist.SelectedIndex = 0;
-
-            if (_playlist_items.Count >= 1)
-                My_Text_No_Playlist.Content = "";
+            //lucas related
+            LibStocker.addMedia(ofd.FileName);
+            this.Bibli_reset();
         }
 
         private void Button_Play(object sender, RoutedEventArgs e)
         {
+            if (MediaReader.Source == null)
+                return ;
             if (MediaReader_Playing == false)
             {
-                if (MediaReader.IsLoaded == false)
-                {
-                    MessageBox.Show("Veuillez selectionner un fichier a lire !");
-                    return ;
-                }
+                this.Blibli_Set_Invis();
                 MediaReader_Playing = true;
-                MediaReader.Play();
                 MediaReader.Play();
                 My_Button_Play.Background = brush_pause;
             }
@@ -149,6 +160,7 @@ namespace WindowsMediaPlayer
         {
             MediaReader_Playing = false;
             My_Button_Play.Background = brush_play;
+            this.Blibli_Set_Visible();
             MediaReader.Stop();
         }
 
@@ -186,7 +198,7 @@ namespace WindowsMediaPlayer
             if (pos >= 1)
             {
                 MediaReader.Source = new Uri(_playlist_items.ElementAt(pos - 1));
-                My_Playist.SelectedIndex = pos + 1;
+                My_Playist.SelectedIndex = pos - 1;
             }
         }
 
@@ -200,6 +212,145 @@ namespace WindowsMediaPlayer
             {
                 MediaReader.Source = new Uri(_playlist_items.ElementAt(pos + 1));
                 My_Playist.SelectedIndex = pos + 1;
+            }
+        }
+
+        private void    Blibli_Set_Invis()
+        {
+            My_Text_Musiques.Visibility = Visibility.Hidden;
+            My_Text_Films.Visibility = Visibility.Hidden;
+            My_Text_Images.Visibility = Visibility.Hidden;
+
+            My_ListBox_Musiques.Visibility = Visibility.Hidden;
+            My_ListBox_Films.Visibility = Visibility.Hidden;
+            My_ListBox_Images.Visibility = Visibility.Hidden;
+        }
+
+        private void Blibli_Set_Visible()
+        {
+            Bibli_reset();
+            My_Text_Musiques.Visibility = Visibility.Visible;
+            My_Text_Films.Visibility = Visibility.Visible;
+            My_Text_Images.Visibility = Visibility.Visible;
+
+            My_ListBox_Musiques.Visibility = Visibility.Visible;
+            My_ListBox_Films.Visibility = Visibility.Visible;
+            My_ListBox_Images.Visibility = Visibility.Visible;
+        }
+
+        private void Bibli_reset()
+        {
+            //on clear les listbox
+            My_ListBox_Musiques.Items.Clear();
+            My_ListBox_Films.Items.Clear();
+            My_ListBox_Images.Items.Clear();
+
+            //on ajoute par rapport aux bails de lucas
+            List<string> my_list = LibStocker.getMusic();
+            for (int i = 0; i < my_list.Count(); i++)
+                My_ListBox_Musiques.Items.Add(my_list.ElementAt(i).Substring(my_list.ElementAt(i).LastIndexOf('\\') + 1));
+
+            my_list = LibStocker.getVideo();
+            for (int i = 0; i < my_list.Count(); i++)
+                My_ListBox_Films.Items.Add(my_list.ElementAt(i).Substring(my_list.ElementAt(i).LastIndexOf('\\') + 1));
+
+            my_list = LibStocker.getPic();
+            for (int i = 0; i < my_list.Count(); i++)
+                My_ListBox_Images.Items.Add(my_list.ElementAt(i).Substring(my_list.ElementAt(i).LastIndexOf('\\') + 1));
+        }
+
+        private void Button_Delete(object sender, RoutedEventArgs e)
+        {
+            if (My_ListBox_Musiques.SelectedIndex != -1)
+            {
+                string sel_item = My_ListBox_Musiques.SelectedItem.ToString();
+
+                LibStocker.removeMedia(sel_item);
+            }
+            else if (My_ListBox_Films.SelectedIndex != -1)
+            {
+                string sel_item = My_ListBox_Films.SelectedItem.ToString();
+
+                LibStocker.removeMedia(sel_item);
+            }
+            else if (My_ListBox_Images.SelectedIndex != -1)
+            {
+                string sel_item = My_ListBox_Images.SelectedItem.ToString();
+
+                LibStocker.removeMedia(sel_item);
+            }
+            this.Bibli_reset();
+        }
+
+
+        private void My_ListBox_Images_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            My_ListBox_Musiques.SelectedIndex = -1;
+            My_ListBox_Films.SelectedIndex = -1;
+
+            if (My_ListBox_Images.SelectedIndex == -1)
+                return;
+
+            string sel_item = My_ListBox_Images.SelectedItem.ToString();
+            //load le media
+            MediaReader.Source = new Uri(LibStocker.getfullpath(sel_item));
+        }
+
+        private void My_ListBox_Films_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            My_ListBox_Musiques.SelectedIndex = -1;
+            My_ListBox_Images.SelectedIndex = -1;
+
+            if (My_ListBox_Films.SelectedIndex == -1)
+                return;
+
+            string sel_item = My_ListBox_Films.SelectedItem.ToString();
+            //load le media
+            MediaReader.Source = new Uri(LibStocker.getfullpath(sel_item));
+        }
+
+        private void My_ListBox_Musiques_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            My_ListBox_Images.SelectedIndex = -1;
+            My_ListBox_Films.SelectedIndex = -1;
+
+            if (My_ListBox_Musiques.SelectedIndex == -1)
+                return;
+
+            string sel_item = My_ListBox_Musiques.SelectedItem.ToString();
+            //load le media
+            MediaReader.Source = new Uri(LibStocker.getfullpath(sel_item));
+        }
+
+        private void Button_Send(object sender, RoutedEventArgs e)
+        {
+            if (MediaReader_Playing == false)
+            {
+                if (My_ListBox_Musiques.SelectedIndex != -1)
+                {
+                    string sel_item = My_ListBox_Musiques.SelectedItem.ToString();
+
+                    //playlist related
+                    My_Playist.Items.Insert(0, sel_item);
+                    _playlist_items.Insert(0, LibStocker.getfullpath(sel_item));
+                    My_Playist.SelectedIndex = 0;
+                }
+                else if (My_ListBox_Films.SelectedIndex != -1)
+                {
+                    string sel_item = My_ListBox_Films.SelectedItem.ToString();
+
+                    My_Playist.Items.Insert(0, sel_item);
+                    _playlist_items.Insert(0, LibStocker.getfullpath(sel_item));
+                    My_Playist.SelectedIndex = 0;
+                }
+                else if (My_ListBox_Images.SelectedIndex != -1)
+                {
+                    string sel_item = My_ListBox_Images.SelectedItem.ToString();
+
+                    My_Playist.Items.Insert(0, sel_item);
+                    _playlist_items.Insert(0, LibStocker.getfullpath(sel_item));
+                    My_Playist.SelectedIndex = 0;
+                }
             }
         }
 
