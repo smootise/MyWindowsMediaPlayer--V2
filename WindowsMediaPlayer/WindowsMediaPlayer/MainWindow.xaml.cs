6378d2fe15da66df42b19f8e6e98f372f7ab65ac
Vroenguard <corentin.bajdek@gmail.com>
2015-01-11 15:29:31 +0100

    Playlist fonctionelle
    Les vidéos s'enchainent toutes seules + bouton next + bouton prev
    fonctionnels

--- WindowsMediaPlayer/WindowsMediaPlayer/MainWindow.xaml.cs
+++ WindowsMediaPlayer/WindowsMediaPlayer/MainWindow.xaml.cs
@@ -32,11 +32,14 @@ namespace WindowsMediaPlayer
     {
         private bool            MediaReader_Playing = false;
         private bool            userIsDraggingSlider = false;
-        private ImageBrush      brush_play;
-        private ImageBrush      brush_pause;
-        private ImageBrush      brush_load;
-        private ImageBrush      brush_stop;
         private List<string>    _playlist_items = new List<string>();
+
+        private ImageBrush brush_play;
+        private ImageBrush brush_pause;
+        private ImageBrush brush_load;
+        private ImageBrush brush_stop;
+        private ImageBrush brush_next;
+        private ImageBrush brush_prev;
 
         public MainWindow()
         {
@@ -46,15 +49,21 @@ namespace WindowsMediaPlayer
             brush_pause = new ImageBrush();
             brush_load = new ImageBrush();
             brush_stop = new ImageBrush();
+            brush_next = new ImageBrush();
+            brush_prev = new ImageBrush();
 
             brush_play.ImageSource = new BitmapImage(new Uri("play_button.png", UriKind.Relative));
             brush_pause.ImageSource = new BitmapImage(new Uri("pause_button.png", UriKind.Relative));
             brush_load.ImageSource = new BitmapImage(new Uri("load_button.png", UriKind.Relative));
             brush_stop.ImageSource = new BitmapImage(new Uri("stop_button.png", UriKind.Relative));
+            brush_next.ImageSource = new BitmapImage(new Uri("next_button.png", UriKind.Relative));
+            brush_prev.ImageSource = new BitmapImage(new Uri("previous_button.png", UriKind.Relative));
 
             My_Button_Play.Background = brush_play;
             My_Button_Load.Background = brush_load;
             My_Button_Stop.Background = brush_stop;
+            My_Button_Next.Background = brush_next;
+            My_Button_Prev.Background = brush_prev;
             if (My_Playist.Items.Count == 0)
                 My_Text_No_Playlist.Content = "Your playlist" + Environment.NewLine + "   is empty.";
 
@@ -71,11 +80,28 @@ namespace WindowsMediaPlayer
                 sliProgress.Minimum = 0;
                 sliProgress.Maximum = MediaReader.NaturalDuration.TimeSpan.TotalSeconds;
                 sliProgress.Value = MediaReader.Position.TotalSeconds;
+
+                // si la vidéo est finie
+                if (MediaReader.Position.TotalSeconds == MediaReader.NaturalDuration.TimeSpan.TotalSeconds)
+                {
+                    string  sel_item = My_Playist.SelectedItem.ToString();
+                    int     pos = My_Playist.Items.IndexOf(sel_item);
+                    //s'il reste au moins un truc a lire dans la playlist
+                    if (pos < (My_Playist.Items.Count - 1))
+                    {
+                        MediaReader.Stop();
+                        MediaReader.Source = new Uri(_playlist_items.ElementAt(pos + 1));
+                        MediaReader_Playing = true;
+                        MediaReader.Play();
+                        My_Playist.SelectedIndex = pos + 1;
+                    }
+                }
             }
         }
 
         private void Button_Load(object sender, RoutedEventArgs e)
         {
+
             OpenFileDialog  ofd;
 
             ofd = new OpenFileDialog();
@@ -84,12 +110,17 @@ namespace WindowsMediaPlayer
             ofd.Filter = "Media (*.*) | *.*";
             ofd.ShowDialog();
 
+            if (_playlist_items.Contains(ofd.FileName) != false)
+                return ;
             MediaReader.Source = new Uri(ofd.FileName);
 
             //playlist related
-            My_Playist.Items.Add(ofd.FileName.Substring(ofd.FileName.LastIndexOf('\\') + 1));
-            _playlist_items.Add(ofd.FileName);
-            My_Text_No_Playlist.Content = "";
+            My_Playist.Items.Insert(0, ofd.FileName.Substring(ofd.FileName.LastIndexOf('\\') + 1));
+            _playlist_items.Insert(0, ofd.FileName);
+            My_Playist.SelectedIndex = 0;
+
+            if (_playlist_items.Count >= 1)
+                My_Text_No_Playlist.Content = "";
         }
 
         private void Button_Play(object sender, RoutedEventArgs e)
@@ -145,6 +176,31 @@ namespace WindowsMediaPlayer
         private void sliProgress_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             lblProgressStatus.Text = TimeSpan.FromSeconds(sliProgress.Value).ToString(@"hh\:mm\:ss");
+        }
+
+        private void Button_Prev(object sender, RoutedEventArgs e)
+        {
+            string  sel_item = My_Playist.SelectedItem.ToString();
+            int     pos = My_Playist.Items.IndexOf(sel_item);
+
+            if (pos >= 1)
+            {
+                MediaReader.Source = new Uri(_playlist_items.ElementAt(pos - 1));
+                My_Playist.SelectedIndex = pos + 1;
+            }
+        }
+
+        private void Button_Next(object sender, RoutedEventArgs e)
+        {
+            string sel_item = My_Playist.SelectedItem.ToString();
+            int pos = My_Playist.Items.IndexOf(sel_item);
+
+            //s'il reste au moins un truc a lire dans la playlist
+            if (pos < (My_Playist.Items.Count - 1))
+            {
+                MediaReader.Source = new Uri(_playlist_items.ElementAt(pos + 1));
+                My_Playist.SelectedIndex = pos + 1;
+            }
         }
 
     }
